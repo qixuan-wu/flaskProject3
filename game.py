7474331392a27d16b4f47393d64d54c491319454
qixuan-wu <t01qw22@abdn.ac.uk>
2023-03-03 01:08:51 +0000

    Initial commit

---
+++ game.py
@@ -0,0 +1,52 @@
+import sqlite3
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+db_name = 'gamesale_data.db'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/Gamelist')
+def customers():
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("select * from Gamelist")
+    rows = cur.fetchall()
+    conn.close()
+    return render_template('Gamelist.html', rows=rows)
+
+@app.route('/Gamelist_details/<Rank>')
+def customer_details(Rank):
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("select * from Gamelist, Gamelist WHERE Rank=?",(Rank))
+    customer = cur.fetchall()
+    conn.close()
+    return render_template('Gamelist_details.html', Gamelist=Gamelist)
+
+@app.route('/Gamesale')
+def orders():
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("select * from Gamesale")
+    rows = cur.fetchall()
+    conn.close()
+    return render_template('Gamesale.html', rows=rows)
+
+@app.route('/Gamesale_details/<id>')
+def order_details(id):
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    cur.execute("select * from Gamesale_detail WHERE Name=?", (Name))
+    order = cur.fetchall()
+    conn.close()
+    return render_template('Gamesale_details.html', Gamesale=Gamesale)
---
+++ set_up.py
@@ -0,0 +1,46 @@
+import csv
+import sqlite3
+
+conn = sqlite3.connect('gamesale_data.db')
+cur = conn.cursor()
+
+conn.execute('DROP TABLE IF EXISTS Gamelist')
+conn.execute('DROP TABLE IF EXISTS Gamesale')
+print("table dropped successfully");
+conn.execute(
+    'CREATE TABLE Gamesale(  Name TEXT PRIMARY KEY, NASales INTEGER, EUSales INTEGER,JPSales INTEGER,OtherSales INTEGER, GlobalSales INTEGER)')
+conn.execute(
+    'CREATE TABLE Gamelist ( Rank INTEGER PRIMARY KEY,Name TEXT , Platform TEXT,Year INTEGER, Genre TEXT,Publisher INTEGER, FOREIGN KEY(Name) REFERENCES Gametale(Name))')
+print("table created successfully");
+with open(r'C:\Users\Dell\PycharmProjects\flaskProject3\table\Gamelist.csv', newline='') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(reader)
+    for row in reader:
+        print(row)
+
+        Rank = int(row[0])
+        Name = str(row[1])
+        Platform = str(row[2])
+        Year = int(row[3])
+        Genre = str(row[4])
+        Publisher = str(row[5])
+        # cur.execute('INSERT INTO Gamelist VALUES (?,?,?,?,?,?)', Rank,Name, Platform, Year, Genre, Publisher))
+        conn.commit()
+print("data parsed successfully")
+
+with open(r'C:\Users\Dell\PycharmProjects\flaskProject3\table\Gamesale.csv', newline='') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(reader)
+    for row in reader:
+        print(row)
+
+        Name = str(row[0])
+        NASales = float(row[1])
+        EUSale = float(row[2])
+        JPSale = float(row[3])
+        OtherSales = float(row[4])
+        GlobalSales = float(row[4])
+        # cur.execute('INSERT INTO Gamesale VALUES (?,?,?,?,?,?)', Name, Platform, Year, Genre, Publisher))
+        conn.commit()
+print("data parsed successfully")
+conn.close()
